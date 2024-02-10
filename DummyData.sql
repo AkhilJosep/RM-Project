d464a098876d204a1efeb0141e98ab2efb3ae7c5
AkhilJosep <154521647+AkhilJosep@users.noreply.github.com>
2024-02-10 17:39:17 +0530

    Add files via upload

---
+++ DummyData.sql
@@ -0,0 +1,51 @@
+use NexusSMSDB_Integration_Final;
+
+insert into StudentRegisters values('S001'),('S002'),('S003'),('S004'),('S005'),('S006'),('S007'),('S008'),('S009'),('S010'),('S011'),('S012'),('S013'),('S014'),('S015'),('S016'),('S017'),('S018')
+
+insert into TeacherRegisters values('T001'),('T002'),('T003'),('T004'),('T005'),('T006'),('T007'),('T008'),('T009'),('T010'),('T011'),('T012'),('T013'),('T014'),('T015'),('T016'),('T017'),('T018')
+
+
+insert into Subjects values('1','Sub1'),('2','Sub2'),('3','Sub3'),('4','Sub4'),('5','Sub5')
+
+insert into Teachers values('T001','Sagar','Babu','1995-07-29','Male','sagarbabu@gmail.com','1'),
+('T002','Akil','A.J','1993-03-01','Male','akhilaj@gmail.com','2'),
+('T003','Gidheon','J','1998-08-15','Male','gidheonj@gmail.com','3'),
+('T004','Harsha','Nair','1997-05-29','Female','harshanair@gmail.com','4'),
+('T005','Faris','K.P','1990-04-18','Male','fariskp@gmail.com','5')
+
+
+insert into Classes values('CLS01','Class_1'),
+('CLS02','Class_2'),
+('CLS03','Class_3'),
+('CLS04','Class_4')
+
+
+('S001','abc','12345','Student'),
+
+
+insert into Users values('S002','def','12345','Student'),('S003','ghi','12345','Student'),('S004','jkl','12345','Student'),insert into Users values('S005','mno','12345','Student')
+select * from Users
+
+insert into Examinations values('EX01','Public','2025-04-04','CLS01'),('EX02','Semester','2024-07-15','CLS01'),('EX03','Public','2025-04-04','CLS02'),('EX04','Semester','2024-07-15','CLS02'), ('EX05','Modal','2025-02-25','CLS03'),('EX06','Modal','2025-02-25','CLS04')
+
+insert into Students values
+('S002','Akhil','A.J','1993-03-01','Male','CLS02'),
+('S003','Gidheon','J','1998-08-15','Male','CLS01'),
+
+
+
+
+select * from Teachers
+select * from Class
+
+
+select * from Users
+select * from Results
+
+
+
+select * from Students
+select * from Classes
+select *from Subjects
+select * from Examinations
+select * from Results
